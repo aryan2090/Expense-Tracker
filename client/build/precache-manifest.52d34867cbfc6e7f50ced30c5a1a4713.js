e9c28a9ed92ba589708abe65f5b4d12921812908
aryan2090 <55028506+aryan2090@users.noreply.github.com>
2020-12-08 15:56:46 +0000

    Add files via upload

---
+++ client/build/precache-manifest.52d34867cbfc6e7f50ced30c5a1a4713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fc6b3372b07c1dded37779b78eceb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08aa07a744d7ddba6122",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08aa07a744d7ddba6122",
+    "url": "/static/js/main.f3d2c206.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
